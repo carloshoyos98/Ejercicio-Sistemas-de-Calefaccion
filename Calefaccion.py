c62330c913781bebb1fce5d621ceb19d5b878955
carloshoyos98 <choyos@cifpfbmoll.eu>
2019-12-22 15:49:37 +0100

    feat: Crear interfaz Calefaccion
    Creo la clase Calefaccion con atributo combustible y métodos vacíos, así
    puede comportarse como una interfaz para otras clases.

---
+++ Calefaccion.py
@@ -0,0 +1,27 @@
+# Este Ejercico es de POO y el principio D de SOLID
+
+from abc import ABCMeta, abstractmethod
+
+# La clase Calefaccion que creamos seria una interfaz
+# Por eso los métodos que definimos están vacíos
+
+class Calefaccion(metaclass = ABCMeta):
+    def __init__(self):
+        self.combustible = None
+    
+    @abstractmethod
+    def encender(self):
+        pass
+
+    @abstractmethod
+    def apagar(self):
+        pass
+
+    @abstractmethod
+    def cargarCombustible(self, unidades):
+        pass
+
+    @abstractmethod
+    def quemarCombustible(self):
+        pass
+    
